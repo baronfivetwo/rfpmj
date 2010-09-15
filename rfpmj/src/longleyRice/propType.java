5d39e703511dd18123481972c3fd7c70687fef95
clay.nathaniel <clay.nathaniel@aa46c800-f11a-8183-4e87-50dd8d026ca9>
2010-09-15 18:37:33 +0000

    Move to trunk

---
+++ rfpmj/src/longleyRice/propType.java
@@ -0,0 +1,105 @@
+package longleyRice;
+
+public class propType {
+	  
+	  private double aref;
+	  private double dist;
+	  private double[] hg; // array length 2
+	  private double wn;
+	  private double dh;
+	  private double ens;
+	  private double gme;
+	  private double zgndreal;
+	  private double zgndimag;
+	  private double[] he; // array length 2
+	  private double[] dl; // array length 2
+	  private double[] the; // array length 2
+	  
+	  public double getAref() {
+		return aref;
+	}
+	public void setAref(double aref) {
+		this.aref = aref;
+	}
+	public double getDist() {
+		return dist;
+	}
+	public void setDist(double dist) {
+		this.dist = dist;
+	}
+	public double[] getHg() {
+		return hg;
+	}
+	public void setHg(double[] hg) {
+		this.hg = hg;
+	}
+	public double getWn() {
+		return wn;
+	}
+	public void setWn(double wn) {
+		this.wn = wn;
+	}
+	public double getDh() {
+		return dh;
+	}
+	public void setDh(double dh) {
+		this.dh = dh;
+	}
+	public double getEns() {
+		return ens;
+	}
+	public void setEns(double ens) {
+		this.ens = ens;
+	}
+	public double getGme() {
+		return gme;
+	}
+	public void setGme(double gme) {
+		this.gme = gme;
+	}
+	public double getZgndreal() {
+		return zgndreal;
+	}
+	public void setZgndreal(double zgndreal) {
+		this.zgndreal = zgndreal;
+	}
+	public double getZgndimag() {
+		return zgndimag;
+	}
+	public void setZgndimag(double zgndimag) {
+		this.zgndimag = zgndimag;
+	}
+	public double[] getHe() {
+		return he;
+	}
+	public void setHe(double[] he) {
+		this.he = he;
+	}
+	public double[] getDl() {
+		return dl;
+	}
+	public void setDl(double[] dl) {
+		this.dl = dl;
+	}
+	public double[] getThe() {
+		return the;
+	}
+	public void setThe(double[] the) {
+		this.the = the;
+	}
+	public int getKwx() {
+		return kwx;
+	}
+	public void setKwx(int kwx) {
+		this.kwx = kwx;
+	}
+	public int getMdp() {
+		return mdp;
+	}
+	public void setMdp(int mdp) {
+		this.mdp = mdp;
+	}
+	private int kwx;
+	  private int mdp;
+
+}
